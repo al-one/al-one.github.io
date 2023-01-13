5a6f67c21e6f73f2eb356979b3d58141349befde
Alone <hi@anlo.ng>
2023-01-13 17:57:10 +0800

    add adb-screen-card

--- static/js/hass/adb-screen-card.js
+++ static/js/hass/adb-screen-card.js
@@ -1,174 +1,126 @@
-import { directive } from 'https://unpkg.com/lit/directive?module';
-import { fireEvent } from 'https://unpkg.com/custom-card-helpers?module';
+import { LitElement, html, css } from "https://unpkg.com/lit-element@2/lit-element.js?module";
+import {
+  hasAction,
+  handleAction
+} from "https://unpkg.com/custom-card-helpers?module";
+import { actionHandler } from "./action-handler-directive.js";
 
-const isTouch = 'ontouchstart' in window || navigator.maxTouchPoints > 0 || navigator.msMaxTouchPoints > 0;
+class AdbScreenCard extends LitElement {
+  static get properties() {
+    return {
+      hass: {},
+      _config: {}
+    };
+  }
 
-class ActionHandler extends HTMLElement {
-    constructor() {
-        super();
-        this.holdTime = 500;
-        this.ripple = document.createElement('mwc-ripple');
-        this.timer = undefined;
-        this.held = false;
-        this.cooldownStart = false;
-        this.cooldownEnd = false;
+  setConfig(config) {
+    if (!config.entity) {
+      throw new Error("You need to define entity");
     }
+    this._config = config;
+  }
 
-    connectedCallback() {
-        Object.assign(this.style, {
-            position: 'absolute',
-            width: isTouch ? '100px' : '50px',
-            height: isTouch ? '100px' : '50px',
-            transform: 'translate(-50%, -50%)',
-            pointerEvents: 'none',
-        });
+  render() {
+    return html`
+      <ha-card>
+        <hui-image
+          .hass=${this.hass}
+          .entity=${this._config.entity}
+          .image=${this._config.image}
+          .stateImage=${this._config.state_image}
+          .stateFilter=${this._config.state_filter}
+          .cameraImage=${this._config.entity}
+          .cameraView=${this._config.camera_view || "live"}
+          .aspectRatio=${this._config.aspect_ratio + ''}
+          @load=${this._onMediaLoad}
+          @click=${this._handleClick}
+          @mousedown=${this._handleClick}
+          @mouseup=${this._handleClick}
+          @action=${this._handleAction}
+          .actionHandler=${actionHandler({
+            hasHold: hasAction(this._config.hold_action),
+            hasDoubleClick: hasAction(this._config.double_tap_action),
+          })}
+        ></hui-image>
+      </ha-card>
+    `;
+  }
 
-        this.appendChild(this.ripple);
-        this.ripple.primary = true;
-
-        ['touchcancel', 'mouseout', 'mouseup', 'touchmove', 'mousewheel', 'wheel', 'scroll'].forEach(ev => {
-            document.addEventListener(
-                ev,
-                () => {
-                    clearTimeout(this.timer);
-                    this.stopAnimation();
-                    this.timer = undefined;
-                },
-                { passive: true },
-            );
-        });
+  _handleAction(ev) {
+    if (this.hass && this.config && ev.detail.action) {
+      handleAction(this, this.hass, this.config, ev.detail.action);
     }
+  }
 
-    bind(element, options) {
-        if (element.actionHandler) {
-            return;
-        }
-        element.actionHandler = true;
-
-        element.addEventListener('contextmenu', (ev) => {
-            const e = ev || window.event;
-            if (e.preventDefault) {
-                e.preventDefault();
-            }
-            if (e.stopPropagation) {
-                e.stopPropagation();
-            }
-            e.cancelBubble = true;
-            e.returnValue = false;
-            return;
-        });
-
-        const clickStart = (ev) => {
-            if (this.cooldownStart) {
-                return;
-            }
-            this.held = false;
-            let x;
-            let y;
-            if (ev.touches) {
-                x = ev.touches[0].pageX;
-                y = ev.touches[0].pageY;
-            } else {
-                x = ev.pageX;
-                y = ev.pageY;
-            }
-
-            this.timer = window.setTimeout(() => {
-                this.startAnimation(x, y);
-                this.held = true;
-            }, this.holdTime);
-
-            this.cooldownStart = true;
-            window.setTimeout(() => (this.cooldownStart = false), 100);
-        };
-
-        const clickEnd = (ev) => {
-            if (this.cooldownEnd || (['touchend', 'touchcancel'].includes(ev.type) && this.timer === undefined)) {
-                return;
-            }
-            clearTimeout(this.timer);
-            this.stopAnimation();
-            this.timer = undefined;
-            if (this.held) {
-                fireEvent(element, 'action', { action: 'hold' });
-            } else if (options.hasDoubleClick) {
-                if ((ev).detail === 1 || ev.type === 'keyup') {
-                    this.dblClickTimeout = window.setTimeout(() => {
-                        fireEvent(element, 'action', { action: 'tap' });
-                    }, 250);
-                } else {
-                    clearTimeout(this.dblClickTimeout);
-                    fireEvent(element, 'action', { action: 'double_tap' });
-                }
-            } else {
-                fireEvent(element, 'action', { action: 'tap' });
-            }
-            this.cooldownEnd = true;
-            window.setTimeout(() => (this.cooldownEnd = false), 100);
-        };
-
-        const handleEnter = (ev) => {
-            if (ev.keyCode === 13) {
-                return clickEnd(ev);
-            }
-        };
-
-        element.addEventListener('touchstart', clickStart, { passive: true });
-        element.addEventListener('touchend', clickEnd);
-        element.addEventListener('touchcancel', clickEnd);
-        element.addEventListener('keyup', handleEnter);
-
-        // iOS 13 sends a complete normal touchstart-touchend series of events followed by a mousedown-click series.
-        // That might be a bug, but until it's fixed, this should make action-handler work.
-        // If it's not a bug that is fixed, this might need updating with the next iOS version.
-        // Note that all events (both touch and mouse) must be listened for in order to work on computers with both mouse and touchscreen.
-        const isIOS13 = /iPhone OS 13_/.test(window.navigator.userAgent);
-        if (!isIOS13) {
-            element.addEventListener('mousedown', clickStart, { passive: true });
-            element.addEventListener('click', clickEnd);
-        }
+  _handleClick(evt) {
+    var ele = evt.target,
+        eos = this._getElementOffset(ele),
+        evx = evt.pageX - eos.x,
+        evy = evt.pageY - eos.y;
+    console.log(evt.type, {ele, evx, evy, evt});
+    if(evt.type === "click")
+    {
+        let dsp = this._getOriginalPoint(evx, evy, ele);
+        this._adbInput(`tap ${dsp.x} ${dsp.y}`);
     }
+  }
 
-    startAnimation(x, y) {
-        Object.assign(this.style, {
-            left: `${x}px`,
-            top: `${y}px`,
-            display: null,
-        });
-        this.ripple.disabled = false;
-        this.ripple.active = true;
-        this.ripple.unbounded = true;
-    }
+  _onMediaLoad(evt) {
+    console.log("onMediaLoad", evt);
+  }
 
-    stopAnimation() {
-        this.ripple.active = false;
-        this.ripple.disabled = true;
-        this.style.display = 'none';
+  _getElementOffset(ele) {
+    var e = ele,
+        x = 0,
+        y = 0;
+    while (e) {
+      x += e.offsetLeft;
+      y += e.offsetTop;
+      if (e.tagName == "BODY") {
+        break;
+      }
+      e = e.offsetParent || null;
     }
+    return {x, y};
+  }
+
+  _getOriginalPoint(x, y, ele) {
+    var ratio = this._config.screen_ratio || 1,
+        size = this._config.screen_size || null;
+    if (size) {
+      let [width, height] = size.split("x");
+      let [xr, yr] = [width / ele.offsetWidth, height / ele.offsetHeight];
+      x *= xr;
+      y *= yr;
+    }
+    else {
+      x *= ratio;
+      y *= ratio;
+    }
+    return {
+      x: parseInt(x),
+      y: parseInt(y),
+    };
+  }
+
+  _toggle(state) {
+    this.hass.callService("homeassistant", "toggle", {
+      entity_id: state.entity_id
+    });
+  }
+
+  _adbInput(input) {
+    var eid = this._config.adb_entity,
+        cmd = 'input ' + input;
+    if (!eid) {
+      throw new Error("You need to define android_tv entity: adb_entity");
+    }
+    this.hass.callService("androidtv", "adb_command", {
+      entity_id: eid,
+      command: cmd
+    });
+    console.log('adb command', eid, cmd);
+  }
 }
 
-customElements.define('action-handler-sensor-button', ActionHandler);
-
-const getActionHandler = () => {
-    const body = document.body;
-    if (body.querySelector('action-handler-sensor-button')) {
-        return body.querySelector('action-handler-sensor-button');
-    }
-
-    const actionhandler = document.createElement('action-handler-sensor-button');
-    body.appendChild(actionhandler);
-
-    return actionhandler;
-};
-
-export const actionHandlerBind = (element, options) => {
-    const actionhandler = getActionHandler();
-    if (!actionhandler) {
-        return;
-    }
-    actionhandler.bind(element, options);
-};
-
-export const actionHandler = directive((options = {}) => (part) => {
-    actionHandlerBind(part.committer.element, options);
-});
+customElements.define("adb-screen-card", AdbScreenCard);
